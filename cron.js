dd21682903f42f86543f8b612eb2d3f8d2a3a1eb
Craig Andersen <craig.andersen@zefr.com>
2017-06-24 04:07:41 -0600

    removing cache files from project

--- cron.js
+++ cron.js
@@ -5,8 +5,9 @@ const fs = require('fs');
 const baseUrl = 'http://swapi.co/api/';
 
 const dataCache = new CronJob({
-    cronTime: '0 0 * * * *',
+    cronTime: '0 30 * * * *',
     onTick: async () => {
+        console.log('cron started');
         const charData = await utils.getAllData(`${baseUrl}people`);
         const planetData = await utils.getAllData(`${baseUrl}planets`);
         const characters = {
@@ -19,6 +20,7 @@ const dataCache = new CronJob({
         };
         fs.writeFileSync('./cache/characters.json', JSON.stringify(characters));
         fs.writeFileSync('./cache/planets.json', JSON.stringify(planets));
+        console.log('cron done');
     },
     runOnInit: false
 });
